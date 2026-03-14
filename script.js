f1701dc8b6204601f36101839209309b010bb90c
samuelmosesdev <73968301+samuelmosesdev@users.noreply.github.com>
2026-03-14 14:03:31 +0100

    Add image popup and refactor hamburger menu logic
    Refactor code to improve organization and add image popup functionality.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* FILTER */
+
 const filterButtons = document.querySelectorAll(".filter-buttons button");
 const items = document.querySelectorAll(".portfolio-item");
 
@@ -15,11 +17,9 @@ items.forEach(item => {
 if(filter === "all"){
 item.style.display = "block";
 }
-
 else if(item.dataset.category === filter){
 item.style.display = "block";
 }
-
 else{
 item.style.display = "none";
 }
@@ -56,37 +56,46 @@ index = 0;
 }
 
 setInterval(changeRole,2000);
-changeRole();const roles = [
-"Graphic Designer",
-"Product Designer",
-"Visual Designer",
-"Brand Designer"
-];
-
-let index = 0;
-const roleText = document.getElementById("roles");
-
-function changeRole(){
-
-roleText.textContent = roles[index];
-
-index++;
-
-if(index >= roles.length){
-index = 0;
-}
-
-}
-
-setInterval(changeRole,2000);
-
 changeRole();
 
-/* HAMBURGER MENU */
+
+
+/* HAMBURGER */
 
 const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("navLinks");
 
 hamburger.addEventListener("click", () => {
 navLinks.classList.toggle("active");
+});
+
+
+/* IMAGE POPUP */
+
+const portfolioImages = document.querySelectorAll(".portfolio-item img");
+const popup = document.getElementById("imagePopup");
+const popupImg = document.getElementById("popupImg");
+const closePopup = document.getElementById("closePopup");
+
+portfolioImages.forEach(image => {
+
+image.addEventListener("click", () => {
+
+popup.style.display = "flex";
+popupImg.src = image.src;
+
+});
+
+});
+
+closePopup.addEventListener("click", () => {
+popup.style.display = "none";
+});
+
+popup.addEventListener("click", (e) => {
+
+if(e.target !== popupImg){
+popup.style.display = "none";
+}
+
 });
